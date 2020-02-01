03808e92d2609db135809cb781281f2708145feb
Drew Dowling <drewdowling37@gmail.com>
2020-02-01 13:27:10 -0800

    basic panels for game and stats

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,17 @@ import           Brick.Widgets.Center
 import           Brick.Widgets.Border
 import           Brick.Widgets.Border.Style
 
+snakePane :: Widget ()
+snakePane = withBorderStyle unicode $ borderWithLabel
+  (str "Snake!")
+  (center (str "Snake is played here") <+> vBorder <+> fill '.')
+
+statsPane :: Widget ()
+statsPane = withBorderStyle unicode
+  $ borderWithLabel (str "Stats") (center (str "Stats go here"))
+
 ui :: Widget ()
-ui = withBorderStyle unicode $ borderWithLabel
-  (str "Hello!")
-  (center (str "Left") <+> vBorder <+> center (str "Right"))
+ui = hLimitPercent 70 snakePane <+> statsPane
 
 main :: IO ()
 main = simpleMain ui
