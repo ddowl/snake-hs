f6328fa09afd1f569c0a14adb9063e4101fa35b2
Drew Dowling <drewdowling37@gmail.com>
2020-02-01 22:14:29 -0800

    move code to library file

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,5 @@
 module Main where
-
 import           Lib
 
-import           Brick
-import           Brick.Widgets.Center
-import           Brick.Widgets.Border
-import           Brick.Widgets.Border.Style
-
-snakePane :: Widget ()
-snakePane = withBorderStyle unicode $ borderWithLabel
-  (str "Snake!")
-  (center (str "Snake is played here") <+> vBorder <+> fill '.')
-
-statsPane :: Widget ()
-statsPane = withBorderStyle unicode
-  $ borderWithLabel (str "Stats") (center (str "Stats go here"))
-
-ui :: Widget ()
-ui = hLimitPercent 70 snakePane <+> statsPane
-
 main :: IO ()
-main = simpleMain ui
+main = runSnake
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
 module Lib
-    ( someFunc
-    ) where
+    ( runSnake
+    )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Brick
+import           Brick.Widgets.Center
+import           Brick.Widgets.Border
+import           Brick.Widgets.Border.Style
+
+snakePane :: Widget ()
+snakePane = withBorderStyle unicode $ borderWithLabel
+    (str "Snake!")
+    (center (str "Snake is played here") <+> vBorder <+> fill '.')
+
+statsPane :: Widget ()
+statsPane = withBorderStyle unicode
+    $ borderWithLabel (str "Stats") (center (str "Stats go here"))
+
+ui :: Widget ()
+ui = hLimitPercent 70 snakePane <+> statsPane
+
+runSnake :: IO ()
+runSnake = simpleMain ui
