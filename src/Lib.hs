88ea1e53169ad1d1aa35b2103c776f22fb8f2db5
Drew Dowling <drewdowling37@gmail.com>
2020-03-15 12:39:17 -0700

    Separate game/stats panes

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,13 +8,17 @@ import           Brick.Widgets.Center
 import           Brick.Widgets.Border
 import           Brick.Widgets.Border.Style
 
+type Coord = (Int, Int)
+data GameState = GameState { pellet :: Coord, snake :: [Coord] }
+
+currState = GameState { pellet = (0, 0), snake = [(5, 3), (5, 4), (5, 5)] }
+
 snakePane :: Widget ()
-snakePane = withBorderStyle unicode $ borderWithLabel
-    (str "Snake!")
-    (center (str "Snake is played here") <+> vBorder <+> fill '.')
+snakePane = withBorderStyle unicodeRounded
+    $ borderWithLabel (str "Snake!") (center (str "Snake is played here"))
 
 statsPane :: Widget ()
-statsPane = withBorderStyle unicode
+statsPane = withBorderStyle unicodeRounded
     $ borderWithLabel (str "Stats") (center (str "Stats go here"))
 
 ui :: Widget ()
