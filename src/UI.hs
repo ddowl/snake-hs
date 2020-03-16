98d8a6a667eac52b6e7391e3ef5ceaf83e28420d
Drew Dowling <drewdowling37@gmail.com>
2020-03-15 19:24:18 -0700

    Draw initial snake state, rearrange functions

--- src/UI.hs
+++ src/UI.hs
@@ -17,28 +17,46 @@ import           Data.List                      ( intercalate )
 type Name = ()
 data Cell = Snake | Food | Empty
 
-snakePane :: Game -> Widget Name
-snakePane g = withBorderStyle BS.unicodeRounded $ B.borderWithLabel
-    (str "Snake!")
-    (C.center (str "Snake is played here"))
+height = 15
+width = 15
 
--- TODO: Vertical limit should be related to the length of stats, with 1 row padding on top and bottom
-statsPane :: Game -> Widget Name
-statsPane g = vLimit 6 $ withBorderStyle BS.unicodeRounded $ B.borderWithLabel
-    (str "Stats")
-    (C.center $ vBox $ map (str . mergeStatLabel) (stats g))
+runSnake :: IO Game
+runSnake = defaultMain app initGame
 
-mergeStatLabel :: (String, Either Int Bool) -> String
-mergeStatLabel (label, Left i ) = label ++ ": " ++ show i
-mergeStatLabel (label, Right b) = label ++ ": " ++ show b
+app :: App Game () Name
+app = App { appDraw         = \g -> [ui g]
+          , appChooseCursor = neverShowCursor
+          , appHandleEvent  = undefined
+          , appStartEvent   = return
+          , appAttrMap      = const theMap
+          }
 
--- I don't like how I can't easily make a union on Int|Bool.
--- They both are typeclasses of Show, so why can't I return a type that impls Show?
-stats :: Game -> [(String, Either Int Bool)]
-stats g = [("Score", Left (g ^. score)), ("Alive", Right (not $ g ^. dead))]
+-- Attribute map
+
+theMap :: AttrMap
+theMap = attrMap
+    V.defAttr
+    [ (snakeAttr   , V.blue `on` V.blue)
+    , (foodAttr    , V.red `on` V.red)
+    , (gameOverAttr, fg V.red `V.withStyle` V.bold)
+    ]
+
+-- Rendering
 
 ui :: Game -> Widget Name
 ui g = hLimitPercent 70 (snakePane g) <+> padLeft (Pad 2) (statsPane g)
+
+snakePane :: Game -> Widget Name
+snakePane g = withBorderStyle BS.unicodeRounded
+    $ B.borderWithLabel (str "Snake!") grid
+  where
+    grid = vBox (map hBox rows)
+    rows = [ cellsInRow r | r <- [height - 1, height - 2 .. 0] ]
+    cellsInRow y = [ drawCoord (x, y) | x <- [0 .. width - 1] ]
+    drawCoord = drawCell . cellAt
+    cellAt c | c `elem` g ^. snake = Snake
+             | c == g ^. pellet    = Food
+             | otherwise           = Empty
 
 drawCell :: Cell -> Widget Name
 drawCell Snake = withAttr snakeAttr cw
@@ -54,21 +72,22 @@ foodAttr = "foodAttr"
 emptyAttr = "emptyAttr"
 gameOverAttr = "gameOver"
 
-theMap :: AttrMap
-theMap = attrMap
-    V.defAttr
-    [ (snakeAttr   , V.blue `on` V.blue)
-    , (foodAttr    , V.red `on` V.red)
-    , (gameOverAttr, fg V.red `V.withStyle` V.bold)
-    ]
+-- TODO: Vertical/Horizontal limits should be related to the length of stats, with 1 row padding on each side
+statsPane :: Game -> Widget Name
+statsPane g = vLimit 6 $ withBorderStyle BS.unicodeRounded $ B.borderWithLabel
+    (str "Stats")
+    (C.center $ vBox $ map (str . mergeStatLabel) (stats g))
 
-app :: App Game () Name
-app = App { appDraw         = \g -> [ui g]
-          , appChooseCursor = neverShowCursor
-          , appHandleEvent  = undefined
-          , appStartEvent   = return
-          , appAttrMap      = const theMap
-          }
+mergeStatLabel :: (String, Either Int Bool) -> String
+mergeStatLabel (label, Left i ) = label ++ ": " ++ show i
+mergeStatLabel (label, Right b) = label ++ ": " ++ show b
 
-runSnake :: IO Game
-runSnake = defaultMain app initGame
+-- I don't like how I can't easily make a union on Int|Bool.
+-- They both are typeclasses of Show, so why can't I return a type that impls Show?
+stats :: Game -> [(String, Either Int Bool)]
+stats g = [("Score", Left (g ^. score)), ("Alive", Right (not $ g ^. dead))]
+
+
+-- Event handling
+-- TODO
+
